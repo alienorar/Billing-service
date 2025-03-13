9ff780bb40e1b09e612ecf7252941de1faae0728
alienorar <rayhonakhayrullaeva@gmail.com>
2025-03-13 14:54:54 +0500

    loader added

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -2,7 +2,7 @@
 
 const Index = () => {
   return (
-      <div className="w-full h-[80vh] flex justify-center items-center m-auto"><p className="text-orange-400 text-[24px]">Loading...</p> <div className="loader"></div></div>
+      <div className="w-full h-[80vh] flex justify-center items-center m-auto"><p className="text-[#1E9FD9] text-[24px]">Loading...</p> <div className="loader"></div></div>
   )
 }
 
